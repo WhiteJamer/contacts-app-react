3f21e94a925ad31c5e946e5083ad23efa67d24ab
whitejamer <mihailkuzin.job@yandex.ru>
2020-06-23 13:53:44 +0300

    Configure routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import ContactsPage from './pages/ContactsPage';
+import AuthPage from './pages/AuthPage';
 
 function App() {
+
+  const isAuth = localStorage.getItem('isAuth') && localStorage.getItem('isAuth') === true
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        {isAuth ?
+          <>
+            <Route path="/" exact>
+              <ContactsPage />
+            </Route>
+            <Redirect to="/" />
+          </>
+          :
+          <>
+            <Route path="/login" exact>
+              <AuthPage />
+            </Route>
+            <Redirect to="/login" />
+          </>
+        }
+      </Switch>
+    </BrowserRouter >
   );
 }
 
